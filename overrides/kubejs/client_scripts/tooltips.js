57971dfcadea25ac0bb747fc48e038bcb6fd3277
adam <57104144+adam9899@users.noreply.github.com>
2026-01-06 18:09:11 +0000

    added tooltips script

---
+++ overrides/kubejs/client_scripts/tooltips.js
@@ -0,0 +1,6 @@
+ItemEvents.modifyTooltips(event => {
+  event.add('rctmod:trainer_card', [
+    '§7Points towards important trainers needed for progression!',
+    '§7Keeps track of your battle progress.'
+  ])
+})
